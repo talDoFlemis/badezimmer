4f6bac912936b27df66209c08626d81ad99bdda9
Said Rodrigues <coderflemis@gmail.com>
2025-11-27 15:55:53 -0300

    feat: add badezimmer logger

---
+++ src/badezimmer/logger.py
@@ -0,0 +1,10 @@
+import logging
+from pythonjsonlogger.json import JsonFormatter
+
+def setup_logger():
+    logger = logging.getLogger()
+    logHandler = logging.StreamHandler()
+    formatter = JsonFormatter()
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
+    logger.setLevel(logging.INFO)
